65c3026310cbcab5f10cda805ae282786c611fb9
tanglei02 <tanglei02@baidu.com>
2018-05-22 15:49:00 +0800

    修改配置

--- template/mip.config.js
+++ template/mip.config.js
@@ -3,26 +3,35 @@
  * @author
  */
 
+
 module.exports = {
+    dev: {
+        /**
+         * 启动mip server调试的端口号
+         *
+         * @type {number}
+         */
+        port: 8111,
 
-    /**
-     * 启动mip server调试的端口号
-     *
-     * @type {number}
-     */
-    port: 8111,
+        /**
+         * 启用调试页面自动刷新
+         *
+         * @type {boolean}
+         */
+        livereload: true,
 
-    /**
-     * 启用调试页面自动刷新
-     *
-     * @type {boolean}
-     */
-    livereload: true,
+        /**
+         * server 启动自动打开页面
+         *
+         * @type {string}
+         */
+        autoopen: '/test/index.html',
 
-    /**
-     * server 启动自动打开页面
-     *
-     * @type {boolean}
-     */
-    autoOpenBrowser: true,
+        /**
+         * mip cli config 文件路径
+         *
+         * @type {string}
+         */
+        config: 'mip.config.js'
+    }
 };
